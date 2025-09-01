1beafde9e31bd6d468832abee7f7b3fb3c71be3d
Samuel Chaves <113139074+Sach1204@users.noreply.github.com>
2025-09-01 13:38:35 -0500

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,21 +1,16 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-public class CalcApp {
-    public static void main(String[] args) throws Exception {
-        // Fuente de entrada: consola (stdin)
-        CharStream input = CharStreams.fromStream(System.in);
 
-        // Etapa léxica
-        labeldExprLexer lexer = new labeldExprLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        // Etapa de análisis sintáctico
-        labeldExprParser parser = new labeldExprParser(tokens);
-        ParseTree tree = parser.prog();
-
-        // Visitador para evaluar las expresiones
-        EvalVisitor visitor = new EvalVisitor();
-        visitor.visit(tree);
-    }
+public class Calc {
+public static void main(String[] args) throws Exception {
+java.io.InputStream is = System.in;
+ANTLRInputStream input = new ANTLRInputStream(is);
+labeldExprLexer lexer = new labeldExprLexer(input);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+labeldExprParser parser = new labeldExprParser(tokens);
+ParseTree tree = parser.prog();
+EvalVisitor eval = new EvalVisitor();
+eval.visit(tree);
+}
 }
